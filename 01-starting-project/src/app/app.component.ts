b375d29470bc1b381e64e15cdf8d3826e72e329c
nadhmii.ayarii@gmail.com <nadhmii.ayarii@gmail.com>
2025-08-14 15:29:41 +0200

    30-using interfaces and types

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import {DUMMY_USERS} from "./dummy-users";
+import {TasksComponent} from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  users = DUMMY_USERS.slice(1,3);
+
+  selectedUserId:string= 'u2';
+  get selectedUser(){
+      return this.users.find((user) => user.id===this.selectedUserId)! //enforce static typing
+    //know which type of value goes where
+    //remove exclamation , ts correctly shows that find might not find a user ==> can return undefined
+    //will cause a problem
+    // simply will crash the application and run to runtime error
+  }
+  onSelectUser(id:any){
+    console.log(id)
+  }
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,22 @@
-import {Component, computed, Input, signal} from '@angular/core';
+import {Component, Input,output} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users";
 
+/*
+type User = {
+  id:string;
+  avatar:string;
+  name:string;
+};
+*/
+
+interface User {
+  id:string;
+  name:string;
+  avatar:string;
+}
 
 const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -13,14 +27,34 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 export class UserComponent {
 
 
+  @Input() user?:({id:string,name:string,avatar:string});
+
+
+  // selectedUser = signal(DUMMY_USERS[randomIndex])
+  // imagePath = computed( ()=> 'assets/users/'+this.selectedUser().avatar )
+
   // when adding ! ==> this indicate that this property is not null
   @Input() avatar! :string;
   @Input() name! :string;
+  @Input() id! :string;
+
+ // @Output() selectedUser = new EventEmitter ();
+  selectedUser= output<string>();
+
+  /*avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(()=>{
+    return 'assets/users/'+this.avatar();
+  });*/
 
   get imagePath(){
     return 'assets/users/'+this.avatar;
   }
+
   onSelectUser() {
-  }
+    this.selectedUser.emit( this.id);
+   }
+
 }
 
